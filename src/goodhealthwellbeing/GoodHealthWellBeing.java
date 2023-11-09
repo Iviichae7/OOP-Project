c127a1656dd9915b55c7be4361c4c0719588a528
Michael Babik <iviichae7@gmail.com>
2023-11-09 00:24:53 +0000

    Started on the GUI elements, not finished yet

--- src/goodhealthwellbeing/GoodHealthWellBeing.java
+++ src/goodhealthwellbeing/GoodHealthWellBeing.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package goodhealthwellbeing;
 
 /**
- *
- * @author michaelbabik
+ * @author Michael Babik
+ * @author Md Hassan
+ * @author Ryan Stokes
  */
 public class GoodHealthWellBeing {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginRegister().setVisible(true);
+        });
     }
     
 }
